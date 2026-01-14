c73e05bffe579224e0747d8fb90d5b71c9af3fb3
py <2663065552@qq.com>
2026-01-14 18:54:30 +0800

    'style:首页模块：修改样式布局'

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -9,7 +9,6 @@ function Home() {
     async function getHomeData() {
       const res = await getHomeDataApi();
       if (res.code === 0) {
-        console.log(res.data);
         var myChart = echarts.init(echartRef.current);
         const series = res.data.source.map(item => {
           return {
@@ -30,7 +29,7 @@ function Home() {
           },
           tooltip: {},
           xAxis: {
-            data: res.data
+            data: [...res.data.date]
           },
           yAxis: {},
           series: series
@@ -45,24 +44,44 @@ function Home() {
       <div className={styles.dataBox}>
         <div className={styles.dataItem}>
           <div className={styles.dataItemContent}>
-            <i className="iconfont icon-dingdan1" style={{ fontSize: '80px', color: '#69b1ff' }}></i>
-            <p>总订单</p>
-            <span>￥25,072</span>
+            <div className={styles.dataItemImg}>
+              <i className="iconfont icon-dingdan1" style={{ fontSize: '50px', color: '#69b1ff' }}></i>
+              <div className={styles.dataItemText}>
+                <p>总订单</p>
+                <span>￥25,072</span>
+              </div>
+            </div>
+            <div className={styles.dataItemLine}></div>
           </div>
           <div className={styles.dataItemContent}>
-            <i className="iconfont icon-xiaoshoue" style={{ color: 'red', fontSize: '80px' }}></i>
-            <p>总销售额</p>
-            <span>￥34,072</span>
+            <div className={styles.dataItemImg}>
+              <i className="iconfont icon-xiaoshoue" style={{ color: 'red', fontSize: '50px' }}></i>
+              <div className={styles.dataItemText}>
+                <p>总销售额</p>
+                <span>￥34,072</span>
+              </div>
+            </div>
+            <div className={styles.dataItemLine}></div>
           </div>
           <div className={styles.dataItemContent}>
-            <i className="iconfont icon-dingdan3" style={{ fontSize: '80px', color: '#95de64' }}></i>
-            <p>今日订单</p>
-            <span>￥54,072</span>
+            <div className={styles.dataItemImg}>
+              <i className="iconfont icon-dingdan3" style={{ fontSize: '50px', color: '#95de64' }}></i>
+              <div className={styles.dataItemText}>
+                <p>今日订单</p>
+                <span>￥54,072</span>
+              </div>
+            </div>
+            <div className={styles.dataItemLine}></div>
           </div>
           <div className={styles.dataItemContent}>
-            <i className="iconfont icon-xiaoshoue1" style={{ color: "orange", fontSize: '80px' }}></i>
-            <p>今日销售额</p>
-            <span>￥98,110</span>
+            <div className={styles.dataItemImg}>
+              <i className="iconfont icon-xiaoshoue1" style={{ color: "orange", fontSize: '50px' }}></i>
+              <div className={styles.dataItemText}>
+                <p>今日销售额</p>
+                <span>￥98,110</span>
+              </div>
+            </div>
+            <div className={styles.dataItemLine}></div>
           </div>
         </div>
         <div className={styles.echart} ref={echartRef}></div>
