40dbff57268aed24427f4a5ec5eb99bf548cf54e
py <2663065552@qq.com>
2026-03-02 11:10:56 +0800

    Updates

---
+++ assets/date-CFCkMWTG.js
@@ -0,0 +1,1 @@
+function e(t){return new Date(t).toLocaleString()}export{e as t};
---
+++ assets/home-D74ckhRI.js
@@ -0,0 +1,1 @@
+import{i as t}from"./request-DJ8UEDwZ.js";const e=()=>t.get("/stats/total"),n=o=>t.get("/users/accountinfo",{params:o});export{e as a,n as g};
