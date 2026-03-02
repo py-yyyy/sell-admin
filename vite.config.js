58787788c81e7e4521127ba930d042829d63b088
py <2663065552@qq.com>
2026-03-02 15:02:21 +0800

    'fix:base'

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     },
   },
-  base: '/sell-admin/',
+  base: './',
   // server: {
   //   port: 5173, // 你的本地端口
   //   proxy: {
