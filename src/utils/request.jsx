de61a0305c8c78088c0640ccb3eef7ddb7a0167b
py <2663065552@qq.com>
2026-03-05 22:19:05 +0800

    'fix:替换baseUrl'

--- src/utils/request.jsx
+++ src/utils/request.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
-export const serverURL = 'http://8.137.157.16:9002';
+// export const serverURL = 'http://8.137.157.16:9002';
+export const serverURL = 'https://api-admin.wled.top';
 
 //统一的请求服务器地址
 const instance = axios.create({
