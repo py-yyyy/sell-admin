b9446d6d8fc3f44453ac6ee7b95b1050416c617d
py-yyyy <py-yyyy@users.noreply.github.com>
2026-03-09 09:01:23 +0000

    'fix:修改baswname的值' 4154015af5b51feaedb34260e0b6c54900a302ab

--- assets/home-C8aqNF0m.js
+++
@@ -1,1 +0,0 @@
-import{i as t}from"./request-D6O_G5qc.js";const e=()=>t.get("/stats/total"),n=o=>t.get("/users/accountinfo",{params:o});export{e as a,n as g};
---
+++ assets/home-dpuUhiCF.js
@@ -0,0 +1,1 @@
+import{i as t}from"./request-Dig1tjeq.js";const e=()=>t.get("/stats/total"),n=o=>t.get("/users/accountinfo",{params:o});export{e as a,n as g};
